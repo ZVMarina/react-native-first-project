8fb371fd10653e2dc74bbcc2d04049e0a82e62af
Marina <zvygintsevame@yandex.ru>
2021-01-21 21:06:51 +0700

    lesson 23. Adding events

--- App.js
+++ App.js
@@ -5,17 +5,7 @@ import { AddTodo } from "./src/AddTodo";
 import { Todo } from "./src/Todo";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, title: "Item 1" },
-    { id: 2, title: "Item 2" },
-    { id: 3, title: "Item 3" },
-    { id: 4, title: "Item 4" },
-    { id: 5, title: "Item 5" },
-    { id: 6, title: "Item 6" },
-    { id: 7, title: "Item 7" },
-    { id: 8, title: "Item 8" },
-    { id: 9, title: "Item 9" },
-  ]); // стейт
+  const [todos, setTodos] = useState([]); // стейт
 
   const addTodo = (title) => {
     // возвращаем новый стейт, который состоит из нового элемента и копии прошлого
@@ -28,6 +18,11 @@ export default function App() {
     ]);
   };
 
+  // метод для удаления
+  const removeTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <Navbar title="Todo App" />
@@ -37,11 +32,8 @@ export default function App() {
         <FlatList
           keyExtractor={(item) => item.id.toString()}
           data={todos}
-          renderItem={({ item }) => {
-            return <Todo todo={item} />;
-          }}
+          renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} />}
         />
-
       </View>
     </View>
   );
